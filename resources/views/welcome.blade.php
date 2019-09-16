a858c77757b5f14e43ae95d76e33cbd8cf215e69
Himanshu Mehru <mehrumj@gmail.com>
2019-09-16 11:22:21 -0700

    fixed the login and register links on the home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
 <h1>Welcome to the PCOF ABFM MOC. </h1>
 <p>Presented to you by Family Medicine Clerkship at the University of Washington School of Medicine</p>
-<p>Please <a href="/login">login</a> or <a href="/register">register</a> to create an account</p>
+<p>Please <a href="{{url('/login')}}">login</a> or <a href="{{url('/register')}}">register</a> to create an account</p>
 </div>
 </div>
 </div>
